6fb4768df315e42552c40ab30bcd77e8350455fa
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-01 18:50:02 +0600

    added continue and break statement

--- index.js
+++ index.js
@@ -44,24 +44,48 @@ else{
 var mark=31
 switch(true){
 case(mark<=100 && mark>=80):
-    console.log('You get A+');
+    //console.log('You get A+');
 break;
 case(mark<=80 && mark>=70):
-    console.log('You get A');
+    //console.log('You get A');
 break;
 case(mark<=70 && mark>=60):
-    console.log('You get A-');
+    //console.log('You get A-');
 break;
 case(mark<=60 && mark>=50):
-    console.log('You get B');
+    //console.log('You get B');
 break;
 case(mark<=50 && mark>=40):
-    console.log('You get C');
+    //console.log('You get C');
 break;
 case(mark<=40 && mark>=33):
-    console.log('You get D');
+    //console.log('You get D');
 break;
             
 default:
-    console.log('you are fail')
+    //console.log('you are fail')
+}
+
+//for loop
+var i;
+for(i=0;i<10;i=i+1 ){
+    //console.log(i+ "<button>Submit</button> </br>")
+}   
+
+//loop continue statement
+var i;
+for(i=0;i<10;i=i+1 ){
+    if(i==5 || i==8){
+        continue;
+    }
+    //console.log(i+ "<button>Submit</button> </br>")
+}
+
+//loop break statement
+var i;
+for(i=0;i<10;i=i+1 ){
+    if(i==5){
+        break;
+    }
+    console.log(i+ "<button>Submit</button> </br>")
 }
