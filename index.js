cd1b4ec8cb8335ea5531cd67d633d8afc1e98b79
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-01 18:18:20 +0600

    added switch case statement

--- index.js
+++ index.js
@@ -18,23 +18,50 @@ else{
 //if else and else if statement
 var mark=97;
 if(mark>=80 && mark<=100){
-    console.log('You got A+')
+    //console.log('You got A+')
 }
 else if(mark<80 && mark>=70){
-    console.log('You got A')
+    //console.log('You got A')
 }
 else if(mark<70 && mark>=60){
-    console.log('You got A-')
+    //console.log('You got A-')
 }
 else if(mark<60 && mark>=50){
-    console.log('You got B')
+    //console.log('You got B')
 }
 else if(mark<50 && mark>=40){
-    console.log('You got C')
+    //console.log('You got C')
 }
 else if(mark<40 && mark>=33){
-    console.log('You got D')
+    //console.log('You got D')
 }
 else{
-   console.log('You fail')
+   //console.log('You fail')
+}
+
+
+//Switch Case statement 
+var mark=31
+switch(true){
+case(mark<=100 && mark>=80):
+    console.log('You get A+');
+break;
+case(mark<=80 && mark>=70):
+    console.log('You get A');
+break;
+case(mark<=70 && mark>=60):
+    console.log('You get A-');
+break;
+case(mark<=60 && mark>=50):
+    console.log('You get B');
+break;
+case(mark<=50 && mark>=40):
+    console.log('You get C');
+break;
+case(mark<=40 && mark>=33):
+    console.log('You get D');
+break;
+            
+default:
+    console.log('you are fail')
 }
